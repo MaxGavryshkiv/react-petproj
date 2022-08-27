a6ce3d62198117447dc65afeb3ad255d4fd52d0f
Max Gavryshkiv <maks20355@gmail.com>
2022-08-27 17:27:29 +0300

    set up project and do client card

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Profile from "./Profile/Profile"
+import user from "./data/user.json";
+    
+function App(){
+    return(<>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    </>)
+}
+
+export default App
